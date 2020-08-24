baf44957a41d8b3e3a743abc651d5a4e3d150d0a
Mustafa Özgün <mustiozgun@gmail.com>
2020-08-24 22:57:41 +0300

    updated react projects

--- src/data.js
+++ src/data.js
@@ -1,86 +1,80 @@
 export default {
-  
-    name: "ozgunmustafa",
-    contactEmail: "ozzgunmustafa@gmail.com",
-    landingPageName: "Mustafa Özgün",
-    landingPageDescription:
-      "Designer and front-end developer. It'ss my portfolio page",
-    projects: [
-      {
-        id: 1, 
-        title: "RoseGang", 
-        description:"Design and Development",
-        images : [
-          "https://i.hizliresim.com/VqYBml.png",
-          "https://i.hizliresim.com/WBCixX.png",
+  name: "ozgunmustafa",
+  contactEmail: "ozzgunmustafa@gmail.com",
+  landingPageName: "Mustafa Özgün",
+  landingPageDescription:
+    "Designer and front-end developer. It'ss my portfolio page",
+  projects: [
+    {
+      id: 1,
+      title: "RoseGang",
+      description: "Design and Development",
+      images: [
+        "https://i.hizliresim.com/VqYBml.png",
+        "https://i.hizliresim.com/WBCixX.png",
+      ],
+      url: "https://codepen.io/ozgunmustafa/pen/oNxYybJ",
+    },
+    {
+      id: 2,
+      title: "Questations",
+      description: "Design and Development",
 
-        ],
-        url: "https://codepen.io/ozgunmustafa/pen/oNxYybJ"
-      },
-      {
-        id: 2, 
-        title: "Questations",
-        description:"Design and Development",
+      images: [
+        "https://i.hizliresim.com/IkHCGR.jpg",
+        "https://i.hizliresim.com/zzPFYI.jpg",
+        "https://i.hizliresim.com/8ZKyff.jpg",
+      ],
 
-        images : [
-          "https://i.hizliresim.com/IkHCGR.jpg",
-          "https://i.hizliresim.com/zzPFYI.jpg",
-          "https://i.hizliresim.com/8ZKyff.jpg",
+      url:
+        "https://smallpdf.com/shared#st=2f45142e-a8e2-4b6e-97fb-1da4450a5446&fn=Questations+Rapor-d%C3%B6n%C3%BC%C5%9Ft%C3%BCr%C3%BCld%C3%BC+%282%29.pdf&ct=1598041993010&tl=share-document&rf=link",
+    },
+    {
+      id: 3,
+      title: "hizliresim.com",
+      description: "Design and Development",
+      images: ["https://i.hizliresim.com/rAREMO.png"],
+      url: "https://hizliresim.com/",
+    },
+    {
+      id: 4,
+      title: "paukampus.com",
+      description: "Design and Development",
 
-        ],
-        
-        url: "https://smallpdf.com/shared#st=2f45142e-a8e2-4b6e-97fb-1da4450a5446&fn=Questations+Rapor-d%C3%B6n%C3%BC%C5%9Ft%C3%BCr%C3%BCld%C3%BC+%282%29.pdf&ct=1598041993010&tl=share-document&rf=link"
-      },
-      {
-        id: 3, 
-        title: "hizliresim.com",
-        description:"Design and Development",
-        images : [
-          "https://i.hizliresim.com/rAREMO.png",
-        ],
-        url: "https://hizliresim.com/"
-      },
-      {
-        id: 4, 
-        title: "paukampus.com",
-        description:"Design and Development",
-        
-        images : [
-          "https://i.hizliresim.com/xRvqHq.png",
-          "https://www.youtube.com/watch?v=AoMAUy8_R_c"
+      images: [
+        "https://i.hizliresim.com/xRvqHq.png",
+        "https://www.youtube.com/watch?v=AoMAUy8_R_c",
+      ],
+      url: "https://www.youtube.com/watch?v=AoMAUy8_R_c",
+    },
+    {
+      id: 5,
+      title: "Responsive Top Fixed Navbar Menu",
+      description: "Design and Development",
+      images: ["https://i.hizliresim.com/bvPR7Q.png"],
+      url: "https://codepen.io/ozgunmustafa/pen/eYZJLzr",
+    },
+    {
+      id: 6,
+      title: "React Projects",
+      description: "Design and Development",
+      images: [
+        "https://i.hizliresim.com/91q4Ne.jpg",
+        "https://i.hizliresim.com/PCjTKR.png",
+        "https://i.hizliresim.com/UguGQg.png",
+        "https://i.hizliresim.com/hOpKqd.png",
+      ],
+      url: "https://github.com/ozgunmustafa",
+    },
+  ],
 
-        ],
-        url: "https://www.youtube.com/watch?v=AoMAUy8_R_c"
-      },
-      {
-        id: 5, 
-        title: "Responsive Top Fixed Navbar Menu",
-        description:"Design and Development",
-        images:[
-            "https://i.hizliresim.com/bvPR7Q.png"
-        ],
-        url: "https://codepen.io/ozgunmustafa/pen/eYZJLzr"
-      },
-       {
-        id: 6, 
-        title: "React Projects",
-        description:"Design and Development",
-        images:[
-            "https://i.hizliresim.com/PCjTKR.png",
-            "https://i.hizliresim.com/UguGQg.png",
-            "https://i.hizliresim.com/hOpKqd.png",
-        ],
-        url: "https://github.com/ozgunmustafa"
-      },
-    ],
-  
-    contact:{
-        LINKEDIN:"https://linkedin.com/in/ozgunmustafa",
-        GITHUB:"https://github.com/ozgunmustafa",
-        CODEPEN:"https://codepen.io/ozgunmustafa",
-        BEHANCE:"https://www.behance.net/ozgunmustafa",
-        EMAIL:"ozzgunmustafa@gmail.com",
-        PHONE:"905073861125",
-        DRIBBBLE:"https://dribbble.com/ozgunmustafa"
-    }
-  };
+  contact: {
+    LINKEDIN: "https://linkedin.com/in/ozgunmustafa",
+    GITHUB: "https://github.com/ozgunmustafa",
+    CODEPEN: "https://codepen.io/ozgunmustafa",
+    BEHANCE: "https://www.behance.net/ozgunmustafa",
+    EMAIL: "ozzgunmustafa@gmail.com",
+    PHONE: "905073861125",
+    DRIBBBLE: "https://dribbble.com/ozgunmustafa",
+  },
+};
