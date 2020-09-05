b874e1345ce7693166c5faf6a9a91d3c7a864c51
ozgunmustafa <34939878+ozgunmustafa@users.noreply.github.com>
2020-09-05 15:42:13 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -82,6 +82,15 @@ export default {
       ],
       url: "https://github.com/ozgunmustafa",
     },
+      {
+      id: 8,
+      title: "birdoktorbul.com",
+      description: "Design",
+      images: [
+        "https://mir-cdn.behance.net/v1/rendition/project_modules/1400/c37bbc103736041.5f53853a6727e.png",
+      ],
+      url: "https://www.behance.net/gallery/103736041/FindDocktor",
+    },
   ],
 
   contact: {
